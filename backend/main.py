6942a8497ca79cfc7c56d9ba843cb0a6240ed2d3
malimass <massimo.malivindi@gmail.com>
2025-08-21 15:10:28 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+from .routers import users, workouts, plans
 
+app = FastAPI(title="Coach Live Platform")
+
+app.include_router(users.router)
+app.include_router(workouts.router)
+app.include_router(plans.router)
